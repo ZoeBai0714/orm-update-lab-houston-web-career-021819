0586719f7afe5b0b57b5340359e3168013a4ff5f
Zoe Bai <s.xue.bai@gmail.com>
2019-03-03 01:16:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,10 +58,11 @@ def self.new_from_db(student)
 end
 
 def self.find_by_name(name)
-  DB[:conn].execute(
+  results = DB[:conn].execute(
     "SELECT * FROM students WHERE name = ?",
     [name]
     )
+  results[0]  
 end
 
 end
